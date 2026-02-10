553bbfdb95b6f455165a10a181c1367b1ce5c52c
Suvi10-s <suvisesh005@gmail.com>
2026-02-10 14:58:07 +0530

    initial commit

---
+++ Tickets.py
@@ -0,0 +1,29 @@
+tickets= {}
+def assign_tickets(ticket_id,title,user_email):
+    if ticket_id not in tickets:
+        tickets[ticket_id] = {'title':title,'user_email':user_email,'status':'open'}
+        print('Ticket assigned')
+    else:
+        print('Ticket already assigned')
+    return
+print(tickets)
+
+def remove_tickets(ticket_id):
+    if ticket_id in tickets:
+        del[tickets[ticket_id]]
+    else:
+        print('ticket not found')
+    return
+
+def update_ticket_status(ticket_id,statusupdation):
+    if ticket_id in tickets:
+        tickets[ticket_id]['status']=statusupdation
+        print('status changed')
+    else:
+        print('ticket not found')
+    return
+print(tickets)
+
+
+
+
